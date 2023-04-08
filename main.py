c0b9588e2be43285ad3741d2349d206c733c70c9
artur-li <arturli.official@gmail.com>
2023-04-08 18:37:29 -0400

    sprites created

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import pygame, sys
 
 # display screen set up
-pygame.display.set_mode((1000, 500))
+screen = pygame.display.set_mode((1000, 500))
 clock = pygame.time.Clock()
 
+# sprite generation
+player1 = pygame.Surface((20,100))
+player1.fill("White")
+player1_rect = player1.get_rect(center=(20,250))
+
+player2 = pygame.Surface((20,100))
+player2.fill("White")
+player2_rect = player2.get_rect(center=(980,250))
+
+ball = pygame.Surface((20,20))
+ball.fill("White")
+ball_rect = ball.get_rect(center=(500,250))
 
 # game loop
 while True:
@@ -15,3 +27,11 @@ while True:
             pygame.quit()
             sys.exit()
 
+    # display sprites to screen
+    screen.blit(player1,player1_rect)
+    screen.blit(player2,player2_rect)
+    screen.blit(ball,ball_rect)
+
+    # update the screen 60fps
+    pygame.display.update()
+    clock.tick(60)
